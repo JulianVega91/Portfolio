7560c756d7fd8f91853733d97bdc3effce6869ce
JulianVega91 <141290008+JulianVega91@users.noreply.github.com>
2026-04-17 18:29:04 -0400

    Actualizar SecurityCheck.java

--- java-projects/SecurityCheck.java
+++ java-projects/SecurityCheck.java
@@ -1,21 +1,81 @@
 import java.util.Scanner;
 
-public class SecurityCheck
-{
-	public static void main(String[] args) {
-	    Scanner sc = new Scanner(System.in);
-	    int count = 0;
-	    boolean lockedOut = false;
-	    
-	    while(count < 3 && !lockedOut) {
-	        System.out.print("Enter password: ");
-	        String pass = sc.next();
-	        if(pass.equals("1234")) {
-	            System.out.println("Access Granted");
-	            break;
-	        }
-	        count++;
-	    }
-	    if(count == 3) System.out.println("Locked Out");
-	}
+public class SecurityCheck {
+
+    public static void main(String[] args)
+    {
+        SecurityCheck seccheck = new SecurityCheck("1234", "abc", "4321");
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean access = false;
+
+        String pw1;
+        String pw2;
+        String overridepw;
+
+        while(!access)
+        {
+            System.out.println("Enter password 1");
+            pw1 = scanner.next();
+
+            System.out.println("Enter password 2");
+            pw2 = scanner.next();
+
+            System.out.println("Enter override password");
+            overridepw = scanner.next();
+
+            access = seccheck.accessGranted(pw1, pw2, overridepw);
+        }
+
+        System.out.println("Access granted!");
+        scanner.close();
+    }
+
+    String userpw1;
+    String userpw2;
+    String overridepw;
+    boolean locked = false;
+    
+    public SecurityCheck(String pw1, String pw2, String overrpw)
+    {
+        userpw1 = pw1;
+        userpw2 = pw2;
+        overridepw = overrpw;
+    }
+    
+    public boolean login1(String trypw1)
+    {
+        if(!userpw1.equals(trypw1))
+        {
+            locked = true;
+            return false;
+        }
+        return true;
+    }
+    
+    public boolean login2(String trypw2)
+    {
+        if(!userpw2.equals(trypw2))
+        {
+            locked = true;
+            return false;
+        }
+        return true;
+    }
+    
+    public boolean adminLogin(String adminPw)
+    {
+        if(overridepw.equals(adminPw))
+        {
+            locked = false;
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean accessGranted(String pw1, String pw2, String overrpw)
+    {
+        return (((login1(pw1) && login2(pw2)) && !locked) || adminLogin(overrpw));
+    }
 }
