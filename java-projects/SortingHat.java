0fcb7b72a3fe87038944b90b975e17bfdf3fb6e9
JulianVega91 <141290008+JulianVega91@users.noreply.github.com>
2026-04-17 18:25:02 -0400

    Actualizar SortingHat.java

--- java-projects/SortingHat.java
+++ java-projects/SortingHat.java
@@ -1,16 +1,61 @@
 import java.util.Scanner;
 
-public class SortingHat
+public class Version2
 {
 	public static void main(String[] args) {
+	    
 	    Scanner sc = new Scanner(System.in);
-	    System.out.println("Sorting Hat Quiz!");
-	    System.out.print("1 for Brave, 2 for Smart: ");
-	    int choice = sc.nextInt();
-	    switch(choice) {
-	        case 1: System.out.println("Gryffindor!"); break;
-	        case 2: System.out.println("Ravenclaw!"); break;
-	        default: System.out.println("Muggle!");
+	    String user = new String("y");
+	    
+	    do
+	    {
+	        System.out.println();
+	        System.out.println("\"Quit\" to stop the program");
+	        System.out.println("You are in the world of Harry Potter, the Sorting Hat is about to tell your house");
+	        System.out.println("But you know the spell to confuse the hat, you just need to chose the house");
+	        System.out.print("Which house would you like to belong to? (Gryffindor, Slytherin, Ravenclaw, Hufflepuff): ");
+	        String place = sc.next();
+	        System.out.println();
+	        
+	        switch(place)
+	        {
+	            case "Gryffindor":
+	            case "gryffindor":
+	                System.out.println("Ah, Gryffindor! Get ready to save the world...\nor at least get into epic trouble.");
+	                break;
+	                
+	            case "Slytherin":
+	            case "slytherin":
+	                System.out.println("Slytherin, huh? ambition and… a touch of mystery.\nJust remember: if you find a snake talking to you in the bathroom, don't ask why.");
+	                break;
+	                
+	            case "Ravenclaw":
+	            case "ravenclaw":
+	                System.out.println("Ravenclaw, brilliant choice! Here the books choose you...\nand yes, you may wake up reciting potions");
+	                break;
+	                
+	            case "Hufflepuff":
+	            case "hufflepuff":
+	                System.out.println("Welcome to Hufflepuff, the coziest house! We have cookies, hugs…\nand a serious nap schedule.");
+	                break;
+	                
+	            case "Quit":
+	            case "quit":
+	                System.out.println("Come on! Even Muggles can last longer in a History of Magic class.");
+	                user = "n";
+	                break;
+	                
+	            default:
+	                System.out.println("Oops, that didn't turn out as you expected! A failed spell?\nBetter try another option before you turn into a toad.");
+	        }
+	        
+	        if(user.equals("y"))
+	        {
+	            System.out.println();
+	            System.out.print("Do you want to continue? (y/n): ");
+	            user = sc.next();
+	        }
 	    }
+	    while(user.equals("y"));
 	}
 }
