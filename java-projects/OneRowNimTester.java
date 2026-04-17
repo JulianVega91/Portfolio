b2b296751a3c3f15906d3ecb4c8cfe15addaa1a8
JulianVega91 <141290008+JulianVega91@users.noreply.github.com>
2026-04-17 16:11:59 -0400

    Create OneRowNimTester.java

---
+++ java-projects/OneRowNimTester.java
@@ -0,0 +1,64 @@
+/**
+ * Lab 10: OneRowNim Game
+ * Logic for a simple stick-taking game.
+ * Demonstrates class creation, private variables, and method interaction.
+ */
+public class OneRowNimTester
+{ 
+  public static void main(String args[])
+  {
+    // Instantiating the game object
+    OneRowNim game = new OneRowNim();
+    
+    // Simulating game turns
+    game.report();
+    game.takeThree();
+    game.report();
+    game.takeThree();
+    game.report();
+    game.takeOne();
+    game.report();
+  } //main()
+}
+
+class OneRowNim
+{ 
+  private int nSticks = 7; // Start with 7 sticks.
+  private int player = 1;  // Player 1 plays first.
+
+  public void takeOne()
+  {
+    takeSticks(1);
+  } // takeOne()
+
+  public void takeTwo()
+  {
+    takeSticks(2);
+  } // takeTwo()
+
+  public void takeThree()
+  {
+    takeSticks(3);
+  }  // takeThree()
+  
+  void takeSticks(int sToTake)
+  {
+    nSticks = nSticks - sToTake;
+    player = 3 - player;
+  }  // takeSticks()
+  
+  public int getSticks()
+  {
+    return nSticks;
+  }
+  
+  public String getReport()
+  {
+    return ("Number of sticks left: " + getSticks() + "\nNext turn by player " + player);
+  }
+
+  public void report()
+  {
+    System.out.println(getReport());
+  }   // report()
+} // OneRowNim class
